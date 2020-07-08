28e5ed24cfebc073ae00d02b5098a981ef8d3e2a
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-07-08 13:41:59 +0900

    [add]_flash.html, [update]customer_controller(customer)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,50 @@
 class CustomersController < ApplicationController
+
+  before_action :authenticate_customer!
+  before_action :set_customer
+  before_action :baria_customer
+
   def edit
   end
 
   def show
   end
 
+  def udpate
+    @customer = current_customer
+    if @customer.update(customer_params)  #更新出来たらマイページ、だめなら編集画面を再表示
+      redirect_to customer_path(@customer), success: 'お客様情報が更新されました！'
+    else
+      flash[:danger] = 'お客様の情報を更新出来ませんでした。空欄の箇所はありませんか？'
+      render :edit
+    end
+  end
+
   def withdraw
   end
 
-  def udpate
+  def withdraw_done
+    @customer = current_customer
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to customer_top_path, info: 'ありがとうございました。またのご利用を心よりお待ちしております。'
+  end
   end
 
-  def withdraw_done
+  private
+
+  def customer_params
+    params.require(:customer).permit(:is_active, :first_name, :first_name_kana, :family_name, :family_name_kana, :post_code, :address, :email, :tel, cart_items_attributes: [:_destroy])
+  end
+
+  def set_customer
+    @customer = current_customer
+  end
+
+  # 他者のページにアクセスしようとすると直前のページに戻る
+  def baria_customer
+    if params[:id].to_i != current_customer.id
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
